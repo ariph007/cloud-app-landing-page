0cae00a59fdb7cc76672969243e61484674f907c
Ari Prayoga Hutabarat <ariprayoga007@gmail.com>
2022-05-24 14:16:33 +0700

    Added Navbar and Hero section

---
+++ src/components/About.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='w-full my-32'>
+      <div className='max-w-[1240px] mx-auto'>
+        <div className='text-center'>
+          <h2 className='text-5xl font-bold'>Trusted by developers across the world</h2>
+          <p className='text-3xl py-6 text-gray-500'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit architecto debitis quibusdam atque illum, labore voluptates et iure eligendi! Voluptates sunt quia delectus nobis veniam. Soluta quis consectetur velit labore?</p>
+        </div>
+        <div>
+          <div>
+            <p>100%</p>
+            <p>Completion</p>
+          </div>
+          <div>
+            <p>24/7%</p>
+            <p>Delivery</p>
+          </div>
+          <div>
+            <p>100K</p>
+            <p>Transactions</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default About
