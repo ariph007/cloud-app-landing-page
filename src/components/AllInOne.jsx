627d9c3452655a7e5772d8311c4f0fbb7a748222
Ari Prayoga Hutabarat <ariprayoga007@gmail.com>
2022-05-25 11:23:36 +0700

    Added Pricing and Footer Section, react-icons and react-scroll package

--- src/components/AllInOne.jsx
+++ src/components/AllInOne.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const AllInOne = () => {
     return (
-        <div className='w-full my-32'>
+        <div name='platforms' className='w-full my-32'>
             <div className='max-w-[1240px] mx-auto px-2'>
                 <h2 className='text-5xl font-bold text-center'>All-In-One-Platform</h2>
                 <p className='text-2xl py-8 text-gray-500 text-center'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus, dolor. Pariatur dignissimos temporibus aperiam eveniet! Molestiae odio architecto accusantium eius voluptatem repellat eveniet vel soluta officiis. Ipsum nemo reprehenderit qui.</p>
